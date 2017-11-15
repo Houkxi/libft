d479492cad7c4c9b7dc6bf8ad174cf304aa079fd
Mathieu MANLEY <mmanley@e2r10p4.42.fr>
2017-11-15 19:00:04 +0100

    Rendu de fonctions partie 2

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanley <mmanley@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/15 18:44:20 by mmanley           #+#    #+#             */
+/*   Updated: 2017/11/15 18:52:41 by mmanley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/proto_ft.h"
+
+void	ft_putnbr_fd(int nb, int fd)
+{
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb *= -1;
+	}
+	if (nb >= 0 && nb <= 9)
+	{
+		ft_putchar_fd(nb + 48, fd);
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+}
