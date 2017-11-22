c8d3c61c27056d1937b3c608d6d693b80357bfd6
Mathieu MANLEY <mmanley@e3r4p15.42.fr>
2017-11-22 18:17:56 +0100

    Rajout de strsplit non fonctionnel et itoa recursive

---
+++ ft_itoa.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanley <mmanley@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 15:48:26 by mmanley           #+#    #+#             */
+/*   Updated: 2017/11/22 18:14:47 by mmanley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <string.h>
+
+int		nbr_count(long n, int i)
+{
+	if (n < 10)
+		return (i);
+	else
+		return (nbr_count(n / 10, i + 1));
+}
+
+char	*nbr_to_str(long n, char *dst, int count)
+{
+	if (n >= 0 && n <= 9)
+	{
+		dst[count] = n + 48;
+	}
+	if (n >= 10)
+	{
+		nbr_to_str(n / 10, dst, count - 1);
+		nbr_to_str(n % 10, dst, count);
+	}
+	return (dst);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*dst;
+	long	sign;
+	int		count;
+	long	i;
+
+	sign = 1;
+	i = n;
+	if (n < 0)
+	{
+		i *= -1;
+		sign = -1;
+	}
+	count = nbr_count(i, 1);
+	if ((dst = (char*)malloc(sizeof(char) * (count + 1))) == 0)
+		return (NULL);
+	dst[count + 1] = '\0';
+	if (sign == -1)
+	{
+		dst[0] = '-';
+		sign = 0;
+	}
+	dst = nbr_to_str(i, dst, count - sign);
+	return (dst);
+}
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,36 +6,43 @@
 /*   By: mmanley <mmanley@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 15:17:38 by mmanley           #+#    #+#             */
-/*   Updated: 2017/11/20 19:18:37 by mmanley          ###   ########.fr       */
+/*   Updated: 2017/11/22 18:15:51 by mmanley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include "../includes/libft.h"
+#include "libft.h"
 
-void	size_word(const char *s, char c, char **tab)
+void	size_word(const char *s, char c, char **tab, int i)
 {
-	int i;
 	int j;
+	int k;
 
-	i = 0;
 	j = 0;
-	while (i < ft_strlen(s) - 1)
+	k = 0;
+	while (s[i])
 	{
-		while (s[i] != c && s[i] != '\0')
+		while (s[i] && s[i] != c)
 		{
-			if (s[i] == c && s[i] != '\0')
+			if (s[i] && s[i] == c)
 				i++;
-		//	printf("%c\n", s[i]);
 			j++;
 			i++;
 		}
-		printf("j = %d\n", j);
-		tab[i] = (char*)malloc(sizeof(char) * (j + 1));
+		if (j != 0)
+		{
+			tab[k] = (char*)malloc(sizeof(char) * (j + 1));
+			ft_bzero(tab[k], j);
+			k++;
+			j = 0;
+		}
 		i++;
-		j = 0;
+	}
+	if (k == 0)
+	{
+		tab[k] = (char*)malloc(sizeof(char) * (1));
+		tab[k][0] = '\0';
 	}
 }
 
@@ -48,7 +55,7 @@ int		count_words(const char *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if ((s[i] == c && s[i+ 1] != '\0') ||
+		if ((s[i] == c && s[i + 1] != '\0' && s[i + 1] != c) ||
 		(wordcount == 0 && s[wordcount] != c))
 		{
 			wordcount++;
@@ -60,37 +67,29 @@ int		count_words(const char *s, char c)
 	return (wordcount);
 }
 
-char	**tab_cpy(char **tab, const char *s, char c)
+char	**tab_cpy(char **tab, const char *s, char c, int i)
 {
-	int i;
 	int j;
 	int k;
 
-	i = 0;
 	j = 0;
 	k = 0;
-	while (tab[k] != '\0')
+	while (s[i])
 	{
-		printf("AvFIN\n");
-		while (s[i] != c && s[i + 1] != '\0')
+		while (s[i] && s[i] != c)
 		{
-			printf("AvPresFIN\n");
-			printf("%d\n", k);
-			printf("%d\n", j);
-			printf("%d\n", i);
-			if (s[i] == c && s[i + 1] != '\0')
-				i++;
-			printf("%c\n", s[i]);
 			tab[k][j] = s[i];
-			printf("%c\n", tab[k][j]);
 			j++;
 			i++;
 		}
-		printf("%s\n", tab[k]);
-		tab[k][j] = '\0';
-		j = 0;
-		i++;
-		k++;
+		while (s[i] && s[i] == c)
+			i++;
+		if (j != 0)
+		{
+			tab[k][j + 1] = '\0';
+			j = 0;
+			k++;
+		}
 	}
 	return (tab);
 }
@@ -104,18 +103,17 @@ char	**ft_strsplit(char const *s, char c)
 
 	tab = NULL;
 	i = 0;
-	j = 0;
-	if (s != NULL )
+	if (!s)
+		return (NULL);
+	j = ft_strlen(s);
+	if (s[i])
 	{
 		wordcounted = count_words(s, c);
-		if (wordcounted == 0)
+		if ((tab = (char**)malloc(sizeof(char*) * (wordcounted + 1))) == NULL)
 			return (NULL);
-		printf("Word count = %d\n", wordcounted);
-		if ((tab = (char**)malloc(sizeof(char*) * (wordcounted))) == NULL)
-			return (NULL);
-		tab[wordcounted] = 0;
-		size_word(s, c, tab);
-		tab_cpy(&tab, s, c);
+		tab[wordcounted + 1] = 0;
+		size_word(s, c, tab, 0);
+		tab_cpy(tab, s, c, 0);
 		return (tab);
 	}
 	return (NULL);
